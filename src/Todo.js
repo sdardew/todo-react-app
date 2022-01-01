64a3e7ad5c84397095337375c3e63f9293d1f81a
sdardew <sdardew@gmail.com>
2022-01-01 23:51:40 +0900

    Add variable about readible

--- src/Todo.js
+++ src/Todo.js
@@ -5,12 +5,19 @@ import { DeleteOutlined } from '@material-ui/icons';
 class Todo extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {item: props.item};
+        this.state = {item: props.item, readOnly: true};
         this.delete = props.delete;
     }
 
     deleteEventHandler = () => {
         this.delete(this.state.item);
+    }
+
+    offReadOnlyMode = () => {
+        console.log("Event!", this.state.readOnly)
+        this.setState({readOnly: false}, () => {
+            console.log("ReadOnly?", this.state.readOnly)
+        });
     }
 
     render() {
@@ -20,9 +27,14 @@ class Todo extends React.Component {
                 <Checkbox checked={item.done}/>
                 <ListItemText>
                     <InputBase
-                        inputProps={{"aria-label": "naked"}}
+                        inputProps={{
+                            "aria-label": "naked",
+                            readOnly: this.state.readOnly,
+                        }}
+                        onClick={this.offReadOnlyMode}
                         type="text"
                         id={item.id}
+                        name={item.id}
                         value={item.title}
                         multiline={true}
                         fullWidth={true}
