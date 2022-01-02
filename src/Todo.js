6e43e687ac67f49194f0e169ae53433e34fda971
sdardew <sdardew@gmail.com>
2022-01-02 16:26:36 +0900

    Add edit function

--- src/Todo.js
+++ src/Todo.js
@@ -11,33 +11,52 @@ class Todo extends React.Component {
 
     deleteEventHandler = () => {
         this.delete(this.state.item);
-    }
+    };
 
     offReadOnlyMode = () => {
-        console.log("Event!", this.state.readOnly)
-        this.setState({readOnly: false}, () => {
-            console.log("ReadOnly?", this.state.readOnly)
-        });
+        // console.log("Event!", this.state.readOnly)
+        this.setState({readOnly: false});
+    };
+
+    enterKeyEventHandler = (e) => {
+        if (e.key === "Enter") {
+            this.setState({readOnly: true});
+        }
+    };
+
+    // item 수정 함수
+    editEventHandler = (e) => {
+        const thisItem = this.state.item;
+        thisItem.title = e.target.value;
+        this.setState({item: thisItem});
+    }
+
+    checkboxEventHandler = (e) => {
+        const thisItem = this.state.item;
+        thisItem.done = !thisItem.done;
+        this.setState({item: thisItem});
     }
 
     render() {
         const item = this.state.item;
         return (
             <ListItem>
-                <Checkbox checked={item.done}/>
+                <Checkbox checked={item.done} onChange={this.checkboxEventHandler}/>
                 <ListItemText>
                     <InputBase
                         inputProps={{
                             "aria-label": "naked",
                             readOnly: this.state.readOnly,
                         }}
-                        onClick={this.offReadOnlyMode}
                         type="text"
                         id={item.id}
                         name={item.id}
                         value={item.title}
                         multiline={true}
                         fullWidth={true}
+                        onClick={this.offReadOnlyMode}
+                        onChange={this.editEventHandler}
+                        onKeyPress={this.enterKeyEventHandler}
                     />
                 </ListItemText>
                 <ListItemSecondaryAction>
